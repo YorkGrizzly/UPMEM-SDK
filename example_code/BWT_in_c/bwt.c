84299b0314645ac4fa38d8d652232a4d9fb69880
YorkGrizzly <140.113.225.118>
2021-01-09 23:06:07 -0500

    2021.01.10 update commmit on sdk repository

--- example_code/BWT_in_c/bwt.c
+++ example_code/BWT_in_c/bwt.c
@@ -1,27 +1,14 @@
-/*
-* String search using the Burrows-Wheeler transform in C
-*
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
-#include <errno.h>
 
-#define VERSION "0.0.1"
-#define MAX_LEN 2048
+
 #define MAX_FILE_LEN 8192
 
-static int mode_bwt_file;
-static int mode_ibwt_file;
-static int mode_bwt_stream;
-static int mode_ibwt_stream;
-static int show_version;
-static int show_help;
 
 typedef struct {
   int num;
-  char str[MAX_LEN];
+  char str[MAX_FILE_LEN];
 } Pair;
 
 /*
@@ -50,15 +37,6 @@ static void create_suffix_array(char *str, int len, char *F, char *L, int *SA) {
     p[i].num = i;
     const char *ctmp = strs;
     strcpy(p[i].str, ctmp);
-
-    // shift
-    /*tmp = strs[len];
-    int j;
-    for (j = len; j > 0; j--) {
-      strs[j] = strs[j - 1];
-    }
-    strs[j] = tmp;*/
-    
     tmp = strs[0];
     int j;
     for (j = 0; j  < len; j++) {
@@ -78,130 +56,96 @@ static void create_suffix_array(char *str, int len, char *F, char *L, int *SA) {
 
   for (int i = 0; i < len + 1; i++) {
     SA[i] = p[i].num;
+    F[i] = p[i].str[0];
+    L[i] = p[i].str[len];
     //printf("{num: %2d, char: '%s'}\n", p[i].num, p[i].str);
-    const char *ptr = &p[i].str[len];
-    const char *preptr;
-
-    if (i != 0) {
-      preptr = &p[i - 1].str[len];
-    } else {
-      preptr = "";
-    }
-
-    // printf("%s:%s\n",ptr, preptr);
 
     // suffix add
-    if (L == NULL) {
-      sprintf(L, "%c", p[i].str[len]);
-    } else {
-      sprintf(L, "%s%c", L, p[i].str[len]);
-    }
+    // if (L == NULL) {
+    //   sprintf(L, "%c", p[i].str[len]);
+    // } else {
+    //   sprintf(L, "%s%c", L, p[i].str[len]);
+    // }
 
-    if (F == NULL) {
-      sprintf(F, "%c", p[i].str[0]);
-    } else {
-      sprintf(F, "%s%c", F, p[i].str[0]);
-    }
+    // if (F == NULL) {
+    //   sprintf(F, "%c", p[i].str[0]);
+    // } else {
+    //   sprintf(F, "%s%c", F, p[i].str[0]);
+    // }
   }
-}
-
-/*
-* get_options
-*
-*/
-static int get_options(int argc, char *const *argv) {
-  int c;
-  while ((c = getopt(argc, argv, "t:s")) != -1) {
-    switch (c) {
-      case 't':
-        //printf("{option: '%c', mode: 'Burrows-Wheeler Transform', input_file: '%s'}\n", optopt, optarg);
-        mode_bwt_file = 1;
-        return 0;
-
-      case 's':
-        //printf("{option: '%c', mode: 'Burrows-Wheeler Transform', input_stream: '%s'}\n", optopt, optarg);
-        mode_bwt_stream = 1;
-        return 0;
-
-      default:
-      return -1;
-    }
-  }
-  return -2;
 }
 
 /*
 * main
 *
 */
-int main(int argc, char **argv) {
-  // Parse options
-  if (get_options(argc, argv) != 0) {
-    printf("[Error] invalid option.\n");
-    return 1;
-  }
-
+int main() {
   // Read File
-  FILE *fp;
   char s[MAX_FILE_LEN];
   int len;
 
-  if (mode_bwt_file || mode_ibwt_file) {
-    if ((fp = fopen(optarg, "r")) == NULL) {
-      printf("[Error] errno %d\n", errno);
-      return errno;
-    }
-    while (fgets(s, MAX_FILE_LEN, fp) != NULL) {
-      len = strlen(s);
-      //printf("{input: %s, length: %d}\n", s, len);
-    }
-    fclose(fp);
+  FILE *f = fopen("input.txt", "r");
+  if(f == NULL){
+	printf("File Opening Error!");
   }
-  char F[len+1];
-  memset(&F, 0, sizeof(F));
+  fgets(s, MAX_FILE_LEN + 1, f);
+  len = strlen(s);
+  fclose(f);
 
-  char L[len+1];
-  memset(&L, 0, sizeof(L));
+  // char F[len+1];
+  // memset(&F, 0, sizeof(F));
+
+  // char L[len+1];
+  // memset(&L, 0, sizeof(L));
   
-  int OCC[len+1][4];
-  memset(&OCC, 0, sizeof(OCC));
+  // int OCC[len+1][4];
+  // memset(&OCC, 0, sizeof(OCC));
 
-  int sample_rate = 40;
+  int sample_rate = 20;
 
-  int sampled_OCC[len/sample_rate + 1][4];
-  memset(&sampled_OCC, 0, sizeof(sampled_OCC));
+  // int sampled_OCC[len/sample_rate + 1][4];
+  // memset(&sampled_OCC, 0, sizeof(sampled_OCC));
+
   
-  int SA[len+1];
-  memset(&SA, 0, sizeof(SA));
   
-  /*if (show_help) {
-    printf("\nOptions:\n");
-    printf("  -t :  Burrows-Wheeler Transform. Input File.\n");
-    printf("  -s :  Burrows-Wheeler Transform. Input Stream.\n");
-    return 0;
-  }
+  // int SA[len+1];
+  // memset(&SA, 0, sizeof(SA));
 
-  if (show_version) {
-    printf("\nVersion:\n%s\n", VERSION);
-    return 0;
-  }*/
 
-  if (mode_bwt_file) {
-    create_suffix_array(s, len, F, L, SA);
+  char *F;
+  F = (char *) malloc((len + 1) * sizeof(char));
+
+  char *L;
+  L = (char *) malloc((len + 1) * sizeof(char));
+
+  int **OCC;
+  OCC = (int **) malloc((len + 1) * sizeof(int *));
+  for(int i = 0; i < len + 1; i++){
+    OCC[i] = (int *) malloc(4 * sizeof(int));
   }
 
+  int *SA;
+  SA = (int *) malloc((len + 1) * sizeof(int));
 
-  if (mode_bwt_stream) {
-    create_suffix_array(optarg, strlen(optarg), F, L, SA);
+  int **sampled_OCC;
+  sampled_OCC = (int **) malloc((len/sample_rate + 1) * sizeof(int *));
+  for(int i = 0; i < len/sample_rate + 1; i++){
+    sampled_OCC[i] = (int *) malloc(4 * sizeof(int));
   }
   
-    int OCC_A = 0;
-    int OCC_C = 0;
-    int OCC_G = 0;
-    int OCC_T = 0;
+ 
+  create_suffix_array(s, len, F, L, SA);
   
-  if(mode_bwt_file || mode_bwt_stream){
-  	for(int i = 0; i <= len; i++){
+
+
+  
+  int OCC_A = 0;
+  int OCC_C = 0;
+  int OCC_G = 0;
+  int OCC_T = 0;
+  
+  
+  for(int i = 0; i <= len; i++){
 		if(L[i] == 'A'){
       OCC_A = OCC_A + 1;
     }
@@ -225,36 +169,181 @@ int main(int argc, char **argv) {
         sampled_OCC[i/sample_rate][j] = OCC[i][j];
     }
 	}
-  } 
 
   int A_offset = 1;
   int C_offset = 1 + OCC_A;
   int G_offset = 1 + OCC_A + OCC_C;
   int T_offset = 1 + OCC_A + OCC_C + OCC_G;
-  
+
+  int sampled_OCC_A[OCC_A/sample_rate + 1][4];
+  memset(&sampled_OCC_A, 0, sizeof(sampled_OCC_A));
+
+  int sampled_OCC_C[(OCC_C - 1)/sample_rate + 1][4];
+  memset(&sampled_OCC_C, 0, sizeof(sampled_OCC_C));
+
+  int sampled_OCC_G[(OCC_G - 1)/sample_rate + 1][4];
+  memset(&sampled_OCC_G, 0, sizeof(sampled_OCC_G));
+
+  int sampled_OCC_T[(OCC_T - 1)/sample_rate + 1][4];
+  memset(&sampled_OCC_T, 0, sizeof(sampled_OCC_T));
+
+
+  for(int i = 0; i < C_offset; i++){
+    int j = i - 0;
+    if(j % sample_rate == 0){
+      for(int k = 0; k < 4; k++){
+        sampled_OCC_A[j/sample_rate][k] = OCC[i][k];
+      }
+    }
+  }
+
+  for(int i = C_offset; i < G_offset; i++){
+    int j = i - C_offset;
+    if(j % sample_rate == 0){
+      for(int k = 0; k < 4; k++){
+        sampled_OCC_C[j/sample_rate][k] = OCC[i][k];
+      }
+    }
+  }
+
+  for(int i = G_offset; i < T_offset; i++){
+    int j = i - G_offset;
+    if(j % sample_rate == 0){
+      for(int k = 0; k < 4; k++){
+        sampled_OCC_G[j/sample_rate][k] = OCC[i][k];
+      }
+    }
+  }
+
+  for(int i = T_offset; i < len + 1; i++){
+    int j = i - T_offset;
+    if(j % sample_rate == 0){
+      for(int k = 0; k < 4; k++){
+        sampled_OCC_T[j/sample_rate][k] = OCC[i][k];
+      }
+    }
+  }
+
+
+
+  FILE *L_table;
+  FILE *SA_table;
+  FILE *sampled_OCC_table;
+
+  L_table = fopen("L_table.txt", "w");
+  fwrite(L, sizeof(char), len + 1, L_table);
+
+  SA_table = fopen("SA_table.txt", "w");
+  for(int i = 0; i < len + 1; i++){
+    fprintf(SA_table, "%d ", SA[i]);
+  }
+
+  sampled_OCC_table = fopen("sampled_OCC_table.txt", "w");
+  for(int i = 0; i < len/sample_rate + 1; i++){
+    for(int j = 0; j < 4; j++){
+      fprintf(sampled_OCC_table, "%d ", sampled_OCC[i][j]);
+    }
+  }
+
+
+  FILE *L_A_table;
+  FILE *sampled_OCC_A_table;
+
+  L_A_table = fopen("L_A_table.txt", "w");
+  fwrite(&L[0], sizeof(char), OCC_A + 1, L_A_table);
+
+  sampled_OCC_A_table = fopen("sampled_OCC_A_table.txt", "w");
+  for(int i = 0; i < OCC_A/sample_rate + 1; i++){
+    for(int j = 0; j < 4; j++){
+      fprintf(sampled_OCC_A_table, "%d ", sampled_OCC_A[i][j]);
+    }
+  }
+
+  FILE *L_C_table;
+  FILE *sampled_OCC_C_table;
+
+  L_C_table = fopen("L_C_table.txt", "w");
+  fwrite(&L[C_offset], sizeof(char), OCC_C, L_C_table);
+
+  sampled_OCC_C_table = fopen("sampled_OCC_C_table.txt", "w");
+  for(int i = 0; i < (OCC_C - 1)/sample_rate + 1; i++){
+    for(int j = 0; j < 4; j++){
+      fprintf(sampled_OCC_C_table, "%d ", sampled_OCC_C[i][j]);
+    }
+  }
+
+  FILE *L_G_table;
+  FILE *sampled_OCC_G_table;
+
+  L_G_table = fopen("L_G_table.txt", "w");
+  fwrite(&L[G_offset], sizeof(char), OCC_G, L_G_table);
+
+  sampled_OCC_G_table = fopen("sampled_OCC_G_table.txt", "w");
+  for(int i = 0; i < (OCC_G - 1)/sample_rate + 1; i++){
+    for(int j = 0; j < 4; j++){
+      fprintf(sampled_OCC_G_table, "%d ", sampled_OCC_G[i][j]);
+    }
+  }
+
+  FILE *L_T_table;
+  FILE *sampled_OCC_T_table;
+
+  L_T_table = fopen("L_T_table.txt", "w");
+  fwrite(&L[T_offset], sizeof(char), OCC_T, L_T_table);
+
+  sampled_OCC_T_table = fopen("sampled_OCC_T_table.txt", "w");
+  for(int i = 0; i < (OCC_T - 1)/sample_rate + 1; i++){
+    for(int j = 0; j < 4; j++){
+      fprintf(sampled_OCC_T_table, "%d ", sampled_OCC_T[i][j]);
+    }
+  }
+
+
   
   //printf("-- Result --\n%s\n", L);
-  //printf("OCC table:\n");
-  /*printf("SA   F   L   OCC_A   OCC_C   OCC_G   OCC_T\n");
+  printf("-- Result --\n");
+  printf("SA   F   L   OCC_A   OCC_C   OCC_G   OCC_T\n");
   for(int i = 0; i <= len; i++){
-    printf("%2d   %c   %c    %2d      %2d      %2d      %2d\n", SA[i], F[i], L[i], OCC[i][0], OCC[i][1], OCC[i][2], OCC[i][3]);
+    printf("%3d  %c   %c    %3d     %3d     %3d     %3d\n", SA[i], F[i], L[i], OCC[i][0], OCC[i][1], OCC[i][2], OCC[i][3]);
   }
   printf("sampled OCC:\n");
   for(int i = 0; i <= len/sample_rate; i++){
     printf("%2d   %2d   %2d   %2d\n", sampled_OCC[i][0], sampled_OCC[i][1], sampled_OCC[i][2], sampled_OCC[i][3]);
   }
   printf("offset:\n");
-  printf("%d   %d   %d   %d\n", A_offset, C_offset, G_offset, T_offset);*/
-  
+  printf("%2d   %2d   %2d   %2d\n", A_offset, C_offset, G_offset, T_offset);
+
+  printf("sampled OCC_A:\n");
+  for(int i = 0; i <= OCC_A/sample_rate; i++){
+    printf("%2d   %2d   %2d   %2d\n", sampled_OCC_A[i][0], sampled_OCC_A[i][1], sampled_OCC_A[i][2], sampled_OCC_A[i][3]);
+  }
+  printf("sampled OCC_C:\n");
+  for(int i = 0; i <= (OCC_C - 1)/sample_rate; i++){
+    printf("%2d   %2d   %2d   %2d\n", sampled_OCC_C[i][0], sampled_OCC_C[i][1], sampled_OCC_C[i][2], sampled_OCC_C[i][3]);
+  }
+  printf("sampled OCC_G:\n");
+  for(int i = 0; i <= (OCC_G - 1)/sample_rate; i++){
+    printf("%2d   %2d   %2d   %2d\n", sampled_OCC_G[i][0], sampled_OCC_G[i][1], sampled_OCC_G[i][2], sampled_OCC_G[i][3]);
+  }
+  printf("sampled OCC_T:\n");
+  for(int i = 0; i <= (OCC_T - 1)/sample_rate; i++){
+    printf("%2d   %2d   %2d   %2d\n", sampled_OCC_T[i][0], sampled_OCC_T[i][1], sampled_OCC_T[i][2], sampled_OCC_T[i][3]);
+  }
+
+
+  free(F);
+  free(OCC);
+
+
   
 
 
   //QUERYING
-  int query_size = 9;
+  int query_size = 3;
   int search_round = query_size - 1;
   int range_max, range_min;
   char query[query_size];
-  strcpy(query, "ATCGCTACT");
+  strcpy(query, "TCG");
 
   if(query[query_size - 1] == 'A'){
     range_min = A_offset;
@@ -306,7 +395,7 @@ int main(int argc, char **argv) {
       offset = T_offset;
     }
 
-    for(int j = range_min; j <= range_max; j++){
+    /*for(int j = range_min; j <= range_max; j++){
       if(L[j] == target_L){
         if(j % sample_rate <= sample_rate/2){
           update_range_min = offset + sampled_OCC[j/sample_rate][OCC_index] - 1;
@@ -349,9 +438,65 @@ int main(int argc, char **argv) {
         }
       }
       if(L[j] == target_L) break;
+    }*/
+    if(range_min % sample_rate <= sample_rate / 2){
+        update_range_min = offset + sampled_OCC[range_min/sample_rate][OCC_index] - 1;
+        for(int k = 1; k <= range_min % sample_rate; k++){
+            if(L[sample_rate * (range_min/sample_rate) + k] == target_L){
+              update_range_min += 1;
+            }
+        }
+    }
+    else if(range_min % sample_rate > sample_rate / 2){
+        if(range_min/sample_rate + 1 < len/sample_rate + 1){
+          update_range_min = offset + sampled_OCC[range_min/sample_rate + 1][OCC_index] - 1;
+          for(int k = 0; k < sample_rate - range_min % sample_rate; k++){
+            if(L[sample_rate * (range_min/sample_rate + 1) - k] == target_L){
+              update_range_min -= 1;
+            }
+          }
+        }
+        else if(range_min/sample_rate + 1 >= len/sample_rate + 1){
+          update_range_min = offset + sampled_OCC[range_min/sample_rate][OCC_index] - 1;
+          for(int k = 1; k <= range_min % sample_rate; k++){
+            if(L[sample_rate * (range_min/sample_rate) + k] == target_L){
+              update_range_min += 1;
+            }
+          }
+        }
+    }
+    if(L[range_min] != target_L) update_range_min += 1;
+
+
+    if(range_max % sample_rate <= sample_rate / 2){
+        update_range_max = offset + sampled_OCC[range_max/sample_rate][OCC_index] - 1;
+        for(int k = 1; k <= range_max % sample_rate; k++){
+            if(L[sample_rate * (range_max/sample_rate) + k] == target_L){
+              update_range_max += 1;
+            }
+        }
+    }
+    else if(range_max % sample_rate > sample_rate / 2){
+        if(range_max/sample_rate + 1 < len/sample_rate + 1){
+          update_range_max = offset + sampled_OCC[range_max/sample_rate + 1][OCC_index] - 1;
+          for(int k = 0; k < sample_rate - range_max % sample_rate; k++){
+            if(L[sample_rate * (range_max/sample_rate + 1) - k] == target_L){
+              update_range_max -= 1;
+            }
+          }
+        }
+        else if(range_max/sample_rate + 1 >= len/sample_rate + 1){
+          update_range_max = offset + sampled_OCC[range_max/sample_rate][OCC_index] - 1;
+          for(int k = 1; k <= range_max % sample_rate; k++){
+            if(L[sample_rate * (range_max/sample_rate) + k] == target_L){
+              update_range_max += 1;
+            }
+          }
+        }
     }
 
-    if(update_range_min == -1 && update_range_max == -1){
+    printf("updated range_min: %d, updated range_max: %d\n", update_range_min, update_range_max);
+    if(update_range_min > update_range_max){
       not_found_flag = 1;
       break;
     }
@@ -370,33 +515,5 @@ int main(int argc, char **argv) {
   }
   else printf("not found");
   printf("\n");
-  /*FILE *f1 = fopen("dpu1.txt", "w");
-  if(f1 == NULL){
-	printf("File Opening Error!");
-  }
-  fwrite(&L[1], sizeof(char), OCC_A, f1);
-  fclose(f1);
-  
-  
-  FILE *f2 = fopen("dpu2.txt", "w");
-  if(f2 == NULL){
-	printf("File Opening Error!");
-  }
-  fwrite(&L[OCC_A+1], sizeof(char), OCC_C, f2);
-  fclose(f2);
-  
-  FILE *f3 = fopen("dpu3.txt", "w");
-  if(f3 == NULL){
-	printf("File Opening Error!");
-  }
-  fwrite(&L[OCC_A+OCC_C+1], sizeof(char), OCC_G, f3);
-  fclose(f3);
-  
-  FILE *f4 = fopen("dpu4.txt", "w");
-  if(f4 == NULL){
-	printf("File Opening Error!");
-  }
-  fwrite(&L[OCC_A+OCC_C+OCC_G+1], sizeof(char), OCC_T, f4);
-  fclose(f4);*/
   return 0;
 }
